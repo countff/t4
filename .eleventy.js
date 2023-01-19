2f2a73077723781c29304096316603faa7d18e5e
countff <countff@n-t.io>
2023-01-19 12:12:17 +0300

    10

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,12 @@ module.exports = function (eleventyConfig) {
 
   // Folders to copy to output folder
   eleventyConfig.addPassthroughCopy("css");
+  return {
+    pathPrefix: "t4"/*,
+    dir: {
+      input: "src",
+      output: "www",
+    }*/
+  };
+
 };
